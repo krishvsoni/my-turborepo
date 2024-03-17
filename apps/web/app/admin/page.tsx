67aa7b886bf39b79d3da77f47d3569837cec81d6
krishvsoni <sonikrish994@gmail.com>
2024-03-17 19:34:35 +0530

    cache

---
+++ apps/web/app/admin/page.tsx
@@ -0,0 +1,7 @@
+import { Admin, Admin as UIAdmin } from "@repo/ui/admin";
+
+export function AdminPage(){
+    return <div>
+        Hi from admin page 
+    </div>
+};
---
+++ packages/ui/src/admin.tsx
@@ -0,0 +1,9 @@
+export function Admin() {
+    return 
+        <div>
+            This is Admin
+
+        </div>
+    
+
+}
